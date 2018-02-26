4126db4dfc538dc69128674efbc9ef7e31b72460
NarayanaWalters <waltersnv@appstate.edu>
2018-02-25 20:26:56 -0500

    footsteps play based on distance travelled instead of time spent moving now

--- characters/player/mover.gd
+++ characters/player/mover.gd
@@ -1,39 +1,35 @@
 extends Node2D
 
 const MOVE_SPEED = 1 # meters per second
-const STEP_RATE = 0.5 # steps per second
+const STEP_RATE = 32 # pixels travelled per step
 const FOOTSTEPS = ["footstep_1", "footstep_2", "footstep_3"] #footstep sounds
 
 var audio_folder = "res://characters/player/player_audio/"
 
-var time_since_last_step = 0
+var last_pos = Vector2()
+var distance_since_last_step = 0
 
 onready var move_ray = $MoveRay #get_node("MoveRay") # collision checking
 onready var step_player = $StepPlayer #get_node("StepPlayer")
 
 func _ready():
-	pass
-
-#var line = [Vector2(), Vector2()]
-#func _draw():
-#	draw_line(line[0], line[1] * 100, Color(1, 1, 1), 2)
+	last_pos = global_position
 
 # dir_vector: direction to move
 # delta: time since last frame
 func move_body(var kine_body, var dir_vector, var delta):
+	var last_pos = global_position
 	var rot = atan2(dir_vector.x, dir_vector.y)
 	var our_rot = global_rotation
 	
-	#line[1] = move_ray.get_cast_to().rotated(rot * -1) # for debug purposes
-	#update()
-	
 	if can_move(rot) && dir_vector.length_squared() > 0:
-		time_since_last_step += delta
-		attempt_step()
 		signal_new_position()
 		kine_body.move_and_collide(dir_vector.rotated(our_rot) * MOVE_SPEED)
-	#else:
-	#	bump_away_from_walls(kine_body)
+		attempt_step()
+	#print("cur: %s last: %s" % [cur_pos, last_pos])
+	var cur_pos = global_position
+	distance_since_last_step += cur_pos.distance_to(last_pos)
+
 
 func bump_away_from_walls(var body):
 	if move_ray.is_colliding():
@@ -46,8 +42,10 @@ func can_move(var rot):
 	return !move_ray.is_colliding()
 
 func attempt_step():
-	if (time_since_last_step > STEP_RATE):
-		time_since_last_step -= STEP_RATE
+	if (distance_since_last_step >= STEP_RATE):
+		var before = distance_since_last_step
+		distance_since_last_step -= STEP_RATE
+		#distance_since_last_step = fposmod(distance_since_last_step, STEP_RATE)
 		var step_index = randi() % FOOTSTEPS.size()
 		var sample = load(audio_folder + FOOTSTEPS[step_index] + ".wav")
 		step_player.stream = sample
